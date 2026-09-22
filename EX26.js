c7fa8be0a8e9f2cd9fb027f11f15319878d3f8d1
fattatihafssa <fattatihafssa@gmail.com>
2026-09-22 15:38:38 +0000

    fff

---
+++ EX26.js
@@ -0,0 +1,10 @@
+let numbers = [1,2,3,4,5,6 ]
+  let count = 0 ; 
+
+  for (let i=0; i<numbers.length;i++) {
+     if (numbers[i] % 2 === 0 ) {
+        count++;
+     }
+  }
+ 
+ console.log(count) ; 
