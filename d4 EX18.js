d58e3243c31634083561423443ab06b1696ce291
fattatihafssa <fattatihafssa@gmail.com>
2026-09-22 11:41:40 +0000

    ccc

---
+++ d4 EX18.js
@@ -0,0 +1,10 @@
+let numbers = [-3, 5, -1, 8, 0];
+
+  let count = 0;
+   for ( i = 0 ; i < numbers.length ; i++) {
+     if ( numbers[i] > 0) {
+         count ++ ; 
+     }
+ }
+ 
+ console.log(count) ; 
